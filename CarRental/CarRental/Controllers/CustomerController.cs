1f932cd6114833e905f6d4a12175c184229db4c4
Jirayu <fallen1wingangle@gmail.com>
2017-03-21 13:41:35 +0700

    SearchCustomerUpdate
    Create Search customer page and controller

---
+++ CarRental/CarRental/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult SearchCustomer()
+        {
+            return View();
+        }
+
+    }
+}
