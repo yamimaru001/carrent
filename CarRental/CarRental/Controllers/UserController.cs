b72e38115065043c7ac95bef025d39b23c7a7875
Nuttarika <booss4430@gmail.com>
2017-03-29 15:45:56 +0700

    ของบุช

---
+++ CarRental/CarRental/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CarRental.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult CreateEmployee()
+        {
+            ViewBag.Message = " สร้าง ข้อมูลพนักงาน";
+
+            return View();
+        }
+
+       
+        
+    }
+}
---
+++ CarRental/CarRental/Models/AddViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRental.Models
+{
+  
+    public partial class Products
+    {
+        
+        public class Product
+        {
+            
+            public int ProductId { get; set; }
+            
+            public string Name { get; set; }
+            
+            public string CategoryName { get; set; }
+           
+            public int Price { get; set; }
+        }
+
+
+        private static readonly Products _instance = new Products();
+
+        private Products() { }
+
+        public static Products Instance
+        {
+            get { return _instance; }
+        }
+        public List<Product> ProductList
+        {
+            get { return products; }
+        }
+        private List<Product> products = new List<Product>() 
+        { 
+                new Product() { ProductId = 1, Name = "Product 1", CategoryName = "Category 1", Price=10}, 
+                new Product() { ProductId = 1, Name = "Product 2", CategoryName = "Category 2", Price=5}, 
+                new Product() { ProductId = 1, Name = "Product 3", CategoryName = "Category 3", Price=15}, 
+                new Product() { ProductId = 1, Name = "Product 4", CategoryName = "Category 1", Price=9} 
+        };
+    }
+
+}
